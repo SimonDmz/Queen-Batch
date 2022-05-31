6a9ef3ed7538c7b72d92df9b50bfec83d9b1459c
Eric Thuaud <58465319+EricThuaud@users.noreply.github.com>
2022-05-31 17:23:39 +0200

    Correct hardcoded tempfolder path issue (#33)
    * upgrade version
    
    * initialize temp_folder constant
    
    * remove hardcoded temp dir from xmlutils class
    
    * finalize first push
    
    * correct files deletion
    
    * finalize issue
    
    * log temp dir and remove dead code
    
    * correct catch treatment

--- src/main/java/fr/insee/queen/batch/Constants.java
+++ src/main/java/fr/insee/queen/batch/Constants.java
@@ -1,8 +1,18 @@
 package fr.insee.queen.batch;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Constants {
+
+	private static final Logger logger = LoggerFactory.getLogger(Constants.class);
 	
 	public static final String SCHEMAS_FOLDER_PATH = "/xsd";
 
@@ -21,8 +31,30 @@ public class Constants {
 	public static final String SURVEY_UNIT = "survey_unit";
 	public static final String CAMPAIGN_ID = "campaign.id";
 
+	public static final String TEMP_FOLDER_PATH = System.getProperty("java.io.tmpdir")+"/"+"queen-batch";
+	public static final Path TEMP_FOLDER= getTempDir(TEMP_FOLDER_PATH);
+
 	private Constants() {
 
+	}
+
+	public static Path getTempDir(String pathFolder) {
+		logger.info("temp directory is: "+ pathFolder);
+		Path tempDirPath = null;
+		File dir = new File(pathFolder);
+
+		if(dir.exists()) {
+			tempDirPath = Paths.get(pathFolder);
+		}
+		else {
+			try {
+				tempDirPath = Files.createDirectory(Paths.get(pathFolder));
+			} catch(IOException e) {
+				logger.debug("Temp directory fail to initialize");
+				e.printStackTrace();
+			}
+		}
+		return tempDirPath;
 	}
 	
 }
